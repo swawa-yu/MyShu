4cacb9504ad7ae0eb8e6553393e2d7ed962a2b2e
swawa-yu <swawa.yu@gmail.com>
2025-07-01 23:57:18 +0900

    feat: Implement image export functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import WeeklySchedule from "./components/WeeklySchedule";
 import ScheduleForm from "./components/ScheduleForm";
 import { Button } from "./components/ui/button";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./store";
 import { addEvent } from "./store/scheduleSlice";
+import html2canvas from "html2canvas";
 
 function App() {
   const dispatch = useDispatch();
   const events = useSelector((state: RootState) => state.schedule.events);
+  const scheduleRef = useRef<HTMLDivElement>(null);
 
   // Load schedule from URL on initial render
   useEffect(() => {
@@ -34,15 +36,30 @@ function App() {
     alert("共有URLをクリップボードにコピーしました！");
   };
 
+  // Export schedule as image
+  const exportScheduleAsImage = () => {
+    if (scheduleRef.current) {
+      html2canvas(scheduleRef.current).then(canvas => {
+        const link = document.createElement('a');
+        link.download = 'schedule.png';
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+      });
+    }
+  };
+
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-end mb-4 space-x-2">
         <Button onClick={generateShareableUrl}>共有URLを生成</Button>
+        <Button onClick={exportScheduleAsImage}>画像としてエクスポート</Button>
         <ScheduleForm>
           <Button>スケジュールを追加</Button>
         </ScheduleForm>
       </div>
-      <WeeklySchedule />
+      <div ref={scheduleRef}>
+        <WeeklySchedule />
+      </div>
     </div>
   );
 }
